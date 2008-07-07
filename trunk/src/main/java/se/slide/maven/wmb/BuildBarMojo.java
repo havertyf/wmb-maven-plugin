0488d3411cb9c723ca5ad24f3790b8c173d8a2c0
www.slide.se <www.slide.se@62e47626-7e4f-0410-85cf-45cdbdd072e8>
2008-07-07 21:19:43 +0000

    Added execution of mqsicreatebar.exe.

--- trunk/src/main/java/se/slide/maven/wmb/BuildBarMojo.java
+++ trunk/src/main/java/se/slide/maven/wmb/BuildBarMojo.java
@@ -3,6 +3,7 @@ package se.slide.maven.wmb;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import java.io.*;
 
 /**
  * 
@@ -12,9 +13,21 @@ import org.apache.maven.plugin.MojoFailureException;
 public class BuildBarMojo extends AbstractMojo {
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		// TODO Auto-generated method stub
-
-		getLog().info("build bar..");
+		getLog().info("building bar..");
+		
+		try
+		{
+			//String[] cmd = { "mqsicreatebar", "-version" }; // use for parameters
+			String[] cmd = { "mqsicreatebar" };
+			Process p = Runtime.getRuntime().exec(cmd);
+			p.waitFor();
+			getLog().info(p.exitValue() + "");
+			
+		}
+		catch (Exception error) {
+			getLog().info("Error running mqsicreatebar. Make sure mqsicreatebar.exe is in your path variables. Exception message: " + error.getMessage());
+		}
+		
 	}
 
 }

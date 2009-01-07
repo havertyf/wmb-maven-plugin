516706f2369b358a8d38acc7ab55eba42e8f4ddc
www.slide.se <www.slide.se@62e47626-7e4f-0410-85cf-45cdbdd072e8>
2009-01-07 12:13:21 +0000

    First working version of the plugin with a configurable WMB workspace property.

--- src/main/java/se/slide/maven/wmb/BuildBarMojo.java
+++ src/main/java/se/slide/maven/wmb/BuildBarMojo.java
@@ -2,7 +2,7 @@ package se.slide.maven.wmb;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -24,82 +24,137 @@ public class BuildBarMojo extends AbstractMojo {
 	 * @readonly
 	 */
 	private org.apache.maven.project.MavenProject project;
-	
-    /**
-     * Base directory of the project.
-     * @parameter expression="${basedir}"
-     */
-    private File baseDirectory;
 
-	
+	/**
+	 * Base directory of the project.
+	 * 
+	 * @parameter expression="${basedir}"
+	 */
+	private File baseDirectory;
+
+	/**
+	 * Base directory of the project.
+	 * 
+	 * @parameter expression="${wmbWorkspace}" default-value="src\\wmb"
+	 */
+	private String wmbWorkspace;
+
+	final static String TARGET_FOLDER = "target";
+
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		/*
-		 * Set the workspace folder to our src\wmb folder
-		 * TODO Make this a configurable setting
-		 * 
+		 * Set the workspace folder to our src\wmb folder TODO Make this a
+		 * configurable setting
 		 */
-		String workspace = ".\\src\\wmb";
-		
+		String workspace = "." + File.separator + wmbWorkspace;
+
+		// Set up the target folder
+		createTargetFolderIfNeeded();
+
+		// All folders in the workspace could be a project folder
 		Collection<File> projects = new ArrayList<File>();
-		Collection<String> msgflows = new ArrayList<String>();
-		Collection<String> msgsets = new ArrayList<String>();
-		
-		for (File child : new File(workspace).listFiles())
-		{
-			if (child.isDirectory())
-			{
+		for (File child : new File(workspace).listFiles()) {
+			if (child.isDirectory()) {
 				projects.add(child);
 			}
 		}
-		
-		for (File project : projects)
-		{
-			for (File file : project.listFiles())
-			{
-				getLog().info(file.getPath());
+
+		// For every WMB project folder (we assume this to be true for every
+		// project with a msgflow/mset
+		// TODO What about ESQL-only projects?
+		Collection<WmbProject> wmbProjects = new ArrayList<WmbProject>();
+		for (File project : projects) {
+			WmbProject wmbProject = new WmbProject();
+			wmbProject.setProjectName(project.getName());
+			for (File file : project.listFiles()) {
+				if (file.getName().endsWith("msgflow")
+						|| file.getName().endsWith("mset")) {
+					wmbProject.addProjectFile(file.getName());
+				}
+			}
+			if (!wmbProject.getProjectFiles().isEmpty()) {
+				getLog().info(
+						"Adding WMB project: " + wmbProject.getProjectName());
+				wmbProjects.add(wmbProject);
 			}
 		}
-		
-		getLog().info("Building bar..");
-		
-		String m = "";		
-		
-		try
-		{
-			
-			//String[] cmd = { "mqsicreatebar", "-version" }; // use for parameters
-			String[] cmd = { "mqsicreatebar", "-data C:\\Temp\\IC0541-Sample\\trunk\\src\\wmb", "-cleanBuild", "-b mike.bar", "-o IC0001-Sample\\IC0001_XML_to_IDOC.msgflow", "-p IC0001-Sample", "-o IC0002-Sample\\IC0001_XML_to_IDOC.msgflow", "-p IC0002-Sample" };
-			//Process p = Runtime.getRuntime().exec(cmd);
-			//p.waitFor();
-			//if (p.exitValue() == 1)
-			//getLog().info(p.exitValue() + " >> " + p.getErrorStream().toString());
-			
-			//String command = "mqsicreatebar -data .\\src\\wmb -cleanBuild -b mike.bar -o IC0001-Sample\\IC00012_XML_to_IDOC.msgflow -p IC0001-Sample";
-			
-			String command = "";
-			
-			Runtime rt = Runtime.getRuntime();
-            Process proc = rt.exec(command);
-            
-            int exitVal = proc.waitFor();
-            
-            InputStream stderr = proc.getErrorStream();
-            InputStreamReader isr = new InputStreamReader(stderr);
-            BufferedReader br = new BufferedReader(isr);
-            String line = null;
-            
-            System.out.println("<ERROR>");
-            while ( (line = br.readLine()) != null)
-                System.out.println(line);
-            System.out.println("</ERROR>");
-            
-            System.out.println("Process exitValue: " + exitVal);
 
+		try {
+			String command = "";
+			command += "mqsicreatebar";
+			command += " -data";
+			command += " " + workspace;
+			command += " -cleanBuild";
+			command += " -b " + TARGET_FOLDER + File.separator
+					+ project.getArtifactId() + ".bar";
+
+			for (WmbProject wmbProject : wmbProjects) {
+				command += " -p " + wmbProject.getProjectName();
+				for (String filename : wmbProject.getProjectFiles()) {
+					command += " -o " + wmbProject.getProjectName() + "\\"
+							+ filename;
+				}
+			}
+
+			getLog().info("Executing command: " + command);
+
+			Runtime rt = Runtime.getRuntime();
+			Process process = rt.exec(command);
+
+			int exitVal = process.waitFor();
+
+			if (exitVal > 0) {
+				// Output error
+				String error = "Reported error: " + getOutAndErrStream(process);
+
+				getLog().error(error);
+				throw new MojoFailureException(error);
+			}
 		}
-		catch (Exception error) {
-			getLog().info("Error running mqsicreatebar. Make sure mqsicreatebar.exe is in your path variables. Exception message: " + error.getMessage());
+		// For exec
+		catch (IOException e) {
+			getLog().info("Error executing process: " + e.getMessage());
 		}
-		
+		// For waitFor
+		catch (InterruptedException e) {
+			getLog().info("Error executing mqsicreatebar: " + e.getMessage());
+		}
+
 	}
 
+	private String getOutAndErrStream(Process p) {
+		StringBuffer cmd_out = new StringBuffer("");
+		if (p != null) {
+			BufferedReader is = new BufferedReader(new InputStreamReader(p
+					.getInputStream()));
+			String buf = "";
+			try {
+				while ((buf = is.readLine()) != null) {
+					cmd_out.append(buf);
+					cmd_out.append(System.getProperty("line.separator"));
+				}
+				is.close();
+				is = new BufferedReader(new InputStreamReader(p
+						.getErrorStream()));
+				while ((buf = is.readLine()) != null) {
+					cmd_out.append(buf);
+					cmd_out.append("\n");
+				}
+				is.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return cmd_out.toString();
+	}
+
+	private void createTargetFolderIfNeeded() {
+		File targetFolder = new File(project.getBasedir().getAbsolutePath()
+				+ File.separator + TARGET_FOLDER);
+
+		if (!targetFolder.exists()) {
+			targetFolder.mkdir();
+			getLog().info("Target folder not found, created target folder.");
+		}
+	}
 }
